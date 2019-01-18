36fe0944d94e751ef97c9330af928e7423432213
Zhian Wang <anitazhianwang@gmail.com>
2019-01-18 12:46:34 -0500

    Add Infection and Readmission

--- ILUM/HS_aggregation.R
+++ ILUM/HS_aggregation.R
@@ -1,8 +1,13 @@
 library(dplyr)
 library(magrittr)
+library(openxlsx)
+factortonumeric <- function(var){
+  var <- as.numeric(as.character(var))
+  return(var)
+}
 ## Read Admission Data ---------------------------------------------------------------------------------
 dat <- read.csv("Data/aha_data_main_indicators_2011_2017.csv", header=TRUE) %>% filter(Year %in% c(2016,2017))
-sysupdate <- openxlsx::read.xlsx("Data/AHA_HealthSystem_Amend.xlsx")
+sysupdate <- read.xlsx("Data/AHA_HealthSystem_Amend.xlsx")
 
 # Update System_member
 dat$AHA.ID <- as.character(dat$AHA.ID)
@@ -32,12 +37,13 @@ makeColNamesUserFriendly <- function(df) {
   df
 }
 dat <- makeColNamesUserFriendly(dat)
+
 dat$Medicare_Provider_ID <- as.numeric(as.character(dat$Medicare_Provider_ID))
 
 rm(sysupdate,systbl,makeColNamesUserFriendly)
 
 # Add HMA SYS
-mappinglist <- openxlsx::read.xlsx("D:/GitHub/HMAResearch/HSData/Data/HMA_Mapping_List_201901.xlsx",sheet = "AHA Hospital List 2017")
+mappinglist <- read.xlsx("D:/GitHub/HMAResearch/HSData/Data/HMA_Mapping_List_201901.xlsx",sheet = "AHA Hospital List 2017")
 
 dat2017 <- dat %>% filter(Year == 2017) %>%
   left_join(mappinglist[c(1:3)],by="AHA_ID") %>%
@@ -54,6 +60,7 @@ stru_measures <- read.csv("Data/Structural Measures - Hospital.csv", header = TR
 
 labdat <- inner_join(dat2016, stru_measures[,c(1,11)],by=c("Medicare_Provider_ID" = "Provider.ID"))
 labdat$AHA_ID <- as.numeric(labdat$AHA_ID)
+
 # HMA Members
 hma <- labdat %>% filter(HMA_Member == "Y") %>%
   count(HMA_System_Name,Measure.Response) %>%
@@ -63,7 +70,7 @@ hma <- labdat %>% filter(HMA_Member == "Y") %>%
   tidyr::spread(key = Measure.Response, value = prop)
 
 # top 100 
-top100mapping <- openxlsx::read.xlsx("Data/2017Top100HS_mapping_0117.xlsx",sheet = "Top_Operating_Revenue")
+top100mapping <- read.xlsx("Data/2017Top100HS_mapping_0117.xlsx",sheet = "Top_Operating_Revenue")
 
 # Split by AHA_System_ID and AHA.ID
 top100_sys <- top100mapping[,c(1:6)] %>% filter(!is.na(AHA_SyStem_ID)) %>% 
@@ -86,5 +93,120 @@ top100_hos <- top100mapping %>% filter(!is.na(AHA.ID)) %>%
 
 top100 <- bind_rows(top100_sys,top100_hos) %>% arrange(oprevrank)
 
-top100$elepercent_percentile <- cut(top100$Yes, breaks=c(0,mean(top100$Yes,na.rm = TRUE),1), 
-                                    labels=c("Below Average","Above Agerage"))
+top100$percentile <- cut(top100$Yes, breaks=c(0,mean(top100$Yes,na.rm = TRUE),1), 
+                                    labels=c("Below Average","Above Average"))
+
+hma %<>% left_join(top100mapping[,c(1,4)], by = "HMA_System_Name")
+
+stat <- data.frame(Metrics = "Average % of Hospitals use electrical lab result in Top 100 HS", 
+                   value = mean(top100$Yes,na.rm = TRUE))
+
+list_of_datasets <- list("KeyStat" = stat, "Top100" = top100, "HMA" = hma)
+write.xlsx(list_of_datasets, file = "Result/electrical_lab_result.xlsx")
+
+rm(hma,labdat,list_of_datasets,stat,stru_measures,top100,top100_hos,top100_sys)
+
+
+## Part 2: Readmission -----------------------------------------------------------------------
+readmisson <- read.csv("Data/Unplanned Hospital Visits - Hospital.csv", header = TRUE) %>%
+  filter(Measure.ID == "READM_30_PN") 
+readmisson$Provider.ID <- as.numeric(as.character(readmisson$Provider.ID))
+readmisson$Score <- as.numeric(as.character(readmisson$Score))
+
+readm <- inner_join(dat2017, (readmisson[,c(1,13)] %>% filter(!is.na(Provider.ID))),
+                    by=c("Medicare_Provider_ID" = "Provider.ID"))
+readm$AHA_ID <- as.numeric(readm$AHA_ID)
+
+# HMA Members
+hma <- readm %>% filter(HMA_Member == "Y" & !is.na(Score)) %>%
+  group_by(HMA_System_Name) %>%
+  summarize(
+    avg_readm_score = (Admissions %*% Score)/sum(Admissions))
+
+# top 100 
+# Split by AHA_System_ID and AHA.ID
+top100_sys <- top100mapping[,c(1:6)] %>% filter(!is.na(AHA_SyStem_ID)) %>% 
+  left_join(readm, by = c("AHA_SyStem_ID" = "System_ID")) %>%
+  filter(!is.na(Score)) %>%
+  group_by(oprevrank,a_Name) %>%
+  summarize(
+    avg_readm_score = (Admissions %*% Score)/sum(Admissions))
+
+top100_hos <- top100mapping %>% filter(!is.na(AHA.ID)) %>% 
+  left_join(readm, by = c("AHA.ID" = "AHA_ID")) %>%
+  filter(!is.na(Score)) %>%
+  group_by(oprevrank,a_Name) %>%
+  summarize(
+    avg_readm_score = (Admissions %*% Score)/sum(Admissions))
+
+top100 <- bind_rows(top100_sys,top100_hos) %>% arrange(oprevrank)
+
+cutoff <- quantile(top100$avg_readm_score,na.rm = TRUE, probs = c(0.1,0.25,0.5,0.75,0.9))
+
+
+top100$percentile <- cut(top100$avg_readm_score, breaks=c(0,cutoff,100), 
+                                       labels=c("Below 10%","10 percentile","25 percentile",
+                                                "50 percentile","75 percentile","90 percentile"))
+
+hma %<>% left_join(top100mapping[,c(1,4)], by = "HMA_System_Name")
+
+stat <- data.frame( percentile = row.names(as.data.frame(cutoff)),
+                    value = cutoff)
+
+
+list_of_datasets <- list("Keycutoff" = stat, "Top100" = top100, "HMA" = hma)
+write.xlsx(list_of_datasets, file = "Result/readmission_result.xlsx")
+
+rm(hma,readmisson, readm,list_of_datasets,stat,top100,top100_hos,top100_sys,cutoff)
+
+## Part 3: Infection -----------------------------------------------------------------------
+infection <- read.csv("Data/Complications and Deaths - Hospital.csv", header = TRUE) %>%
+  filter(Measure.ID == "PSI_13_POST_SEPSIS") 
+infection$Provider.ID <- as.numeric(as.character(infection$Provider.ID))
+infection$Score <- as.numeric(as.character(infection$Score))
+
+infec <- inner_join(dat2017, (infection[,c(1,13)] %>% filter(!is.na(Provider.ID))),
+                    by=c("Medicare_Provider_ID" = "Provider.ID"))
+infec$AHA_ID <- as.numeric(infec$AHA_ID)
+
+# HMA Members
+hma <- infec %>% filter(HMA_Member == "Y" & !is.na(Score)) %>%
+  group_by(HMA_System_Name) %>%
+  summarize(
+    avg_infection_score = (Admissions %*% Score)/sum(Admissions))
+
+# top 100 
+# Split by AHA_System_ID and AHA.ID
+top100_sys <- top100mapping[,c(1:6)] %>% filter(!is.na(AHA_SyStem_ID)) %>% 
+  left_join(infec, by = c("AHA_SyStem_ID" = "System_ID")) %>%
+  filter(!is.na(Score)) %>%
+  group_by(oprevrank,a_Name) %>%
+  summarize(
+    avg_infection_score = (Admissions %*% Score)/sum(Admissions))
+
+top100_hos <- top100mapping %>% filter(!is.na(AHA.ID)) %>% 
+  left_join(infec, by = c("AHA.ID" = "AHA_ID")) %>%
+  filter(!is.na(Score)) %>%
+  group_by(oprevrank,a_Name) %>%
+  summarize(
+    avg_infection_score = (Admissions %*% Score)/sum(Admissions))
+
+top100 <- bind_rows(top100_sys,top100_hos) %>% arrange(oprevrank)
+
+cutoff <- quantile(top100$avg_infection_score,na.rm = TRUE, probs = c(0.1,0.25,0.5,0.75,0.9))
+
+
+top100$percentile <- cut(top100$avg_infection_score, breaks=c(0,cutoff,100), 
+                         labels=c("Below 10%","10 percentile","25 percentile",
+                                  "50 percentile","75 percentile","90 percentile"))
+
+hma %<>% left_join(top100mapping[,c(1,4)], by = "HMA_System_Name")
+
+stat <- data.frame( percentile = row.names(as.data.frame(cutoff)),
+                    value = cutoff)
+
+
+list_of_datasets <- list("Keycutoff" = stat, "Top100" = top100, "HMA" = hma)
+write.xlsx(list_of_datasets, file = "Result/infection_result.xlsx")
+
+rm(hma,infection,infec,list_of_datasets,stat,top100,top100_hos,top100_sys,cutoff)
